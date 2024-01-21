fabb3ee00f3507a1b0cc9eae7de9d213323d9f82
lalit kashyap <lalit.kashyap.ug20@nsut.ac.in>
2024-01-22 00:58:20 +0530

    new commits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer=()=>(
     <div className="flex justify-center">
         <footer className="mb-8 mt-4 text-center w-[85%]">
         <hr className="my-4 text-center"></hr>
-        <h1 className="text-2xl font-semibold"><span className="text-orange-400 font-bold pr-2">FOODIE</span> made by Lalit Kashyap<span className='text-orange-400 font-bold pl-2'><FavoriteBorderIcon/></span></h1>
+        <h1 className="text-2xl font-semibold"><span className="text-orange-400 font-bold pr-2">Zwiggy</span> made by Lalit Kashyap<span className='text-orange-400 font-bold pl-2'><FavoriteBorderIcon/></span></h1>
         </footer>
     </div>
 );
